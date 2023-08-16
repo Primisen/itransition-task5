2eb82d54862a548b15d897c5be3bacbf089dd681
Nadia Shokel <primisen17@gmail.com>
2023-08-16 12:40:29 +0200

    Created localization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "react-bootstrap";
+import {useEffect, useState} from "react";
+import {faker} from "@faker-js/faker";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    function updateLanguage(localization) {
+        console.log(localization);
+    }
+
+    // useEffect(() => {
+    //     updateLanguage("en");
+    // }, []);
+
+    return (
+        <div className="App">
+            <header>
+                <div className="Localization">
+                    <Button
+                        onClick={() => {
+                            updateLanguage("en")
+                        }}>
+                        EN
+                    </Button>
+                    <Button
+                        onClick={() => {
+                            updateLanguage("sv")
+                        }}>
+                        SV
+                    </Button>
+                    <Button
+                        onClick={() => {
+                            updateLanguage("uk")
+                        }}>
+                        UK
+                    </Button>
+                </div>
+            </header>
+
+            <div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
